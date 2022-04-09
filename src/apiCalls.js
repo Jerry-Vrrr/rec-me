524032bb507fee5c03de684ba1038a4ebbe1fc65
Madison Law <madisonlaw1@gmail.com>
2022-04-09 11:40:59 -0600

    Flesh out fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,10 @@
 
 
 export const fetchData = () => {
-
-
+  return fetch('http://fe-cors-proxy.herokuapp.com', {
+    headers: {
+      "Target-URL": 'https://tastedive.com/api/similar?k=435194-ConcertT-B82P7E7L&info=1&q=red+hot+chili+peppers'
+    }
+  })
+    .then(response => response.json())
 }
