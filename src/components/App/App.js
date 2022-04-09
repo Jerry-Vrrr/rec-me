2d8ac36088e0e885b104514d580aeaab3787a5d7
Madison Law <madisonlaw1@gmail.com>
2022-04-09 11:41:13 -0600

    Create useEffect hook for API call

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App/_App.scss';
 import { Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Favorites from '../Favorites/Favorites';
 import { FavoriteContext, FavoriteContextProvider } from '../../contexts/FavoriteContext';
+import { fetchData } from '../../apiCalls';
 
 
 const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchData()
+    .then(info => {
+      setData({ mainArtist: info.Similar.Info, relatedArtists: info.Similar.Results})
+    })
+  }, [])
+
+
   return (
     <div className="App">
       <Header />
