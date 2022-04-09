8a5cb02e0d4a0ba0b55e891f7d7626a135870c02
Madison Law <madisonlaw1@gmail.com>
2022-04-09 09:16:30 -0600

    Install Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
+import React, { useState } from 'react';
 import '../App/_App.scss';
+import { Route } from 'react-router-dom';
+import Header from '../Header/Header';
+
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './_Header.scss'
+
+const Header = () => {
+  return (  
+    <h1>Rec Me!</h1>
+  );
+}
+ 
+export default Header;
+
+
