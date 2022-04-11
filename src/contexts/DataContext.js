88328f883aae8b305270a452da4bc0769ffcb4e6
Eric Matlock <ermatlock@gmail.com>
2022-04-11 16:43:07 -0400

    Declare searchQuery, setQuery

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,11 +1,21 @@
 import React, { useState, createContext, useEffect, useAsync } from "react";
 import { fetchData } from "../apiCalls";
 
+const initialItems = ["red hot chili peppers", "nirvana", "the beatles"]
+const getRandomIndex = (array) => {
+  return Math.floor(Math.random() * array.length);
+};
+
 const DataContext = createContext();
 
 const DataContextProvider = ({ children }) => {
+
   const [data, setData] = useState(null);
-  const [searchQuery, setQuery] = useState(null); 
+  const [searchQuery, setQuery] = useState(""); 
+  useEffect(() => {
+    setQuery(initialItems[getRandomIndex(initialItems)])
+  }, [searchQuery])
+  
   useEffect(() => {
     fetchData(searchQuery).then((info) => {
       setData({
@@ -13,7 +23,7 @@ const DataContextProvider = ({ children }) => {
         relatedItems: info.Similar.Results,
       });
     });
-  }, []);
+  }, [searchQuery]);
 
   return (
     <DataContext.Provider value={{ data }}>
