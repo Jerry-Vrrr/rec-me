258e913709c8ee24f2ab5ecd3eda535eb89f1c7d
Eric Matlock <ermatlock@gmail.com>
2022-04-11 15:59:20 -0400

    Add small bubbles

--- src/components/BubbleBox/BubbleBox.js
+++ src/components/BubbleBox/BubbleBox.js
@@ -1,22 +1,38 @@
-import React, { useContext } from 'react'
-import './_BubbleBox.scss';
-import BigBubble from '../BigBubble/BigBubble';
-import {DataContext} from '../../contexts/DataContext'
-  
+import React, { useContext } from "react";
+import "./_BubbleBox.scss";
+import BigBubble from "../BigBubble/BigBubble";
+import { DataContext } from "../../contexts/DataContext";
+import SmallBubble from "../SmallBubble/SmallBubble";
 
-const BubbleBox =  () => {
-  const data = useContext(DataContext)
-  const artist = data.data.mainArtist
-  // console.log(artist)
+const BubbleBox = () => {
+  const data = useContext(DataContext);
+  const mainItem = data.data.mainItem;
+  const relatedItems = data.data.relatedItems;
+  const createBubbles = () => {
+    return relatedItems.map(item => {
+      let id = Date.now()
+      item.id = id
+      item.key = id
+      return <SmallBubble
+        item={item}
+      />
+    })
+  } 
+
   return (
     <div className="bubble-box">
-      <section className='banner'>
-        <img className='banner' src='https://www.synometrix.com/wp-content/uploads/2020/05/Light-Up-Beach-Ball-4.jpg'></img>
+      <section className="banner">
+        <img
+          className="banner"
+          src="https://www.synometrix.com/wp-content/uploads/2020/05/Light-Up-Beach-Ball-4.jpg"
+        ></img>
       </section>
-      {/* {data && <p>{artist.Name}</p>} */}
-      <BigBubble /> 
+      <BigBubble />
+      <div className="baby-bubble-wrap">
+        {createBubbles()}
+      </div>
     </div>
   );
-}
+};
 
 export default BubbleBox;
