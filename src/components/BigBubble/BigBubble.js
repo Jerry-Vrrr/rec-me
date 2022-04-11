f7bc22ceba14786df232514cfac3e1688ab51199
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-10 21:20:06 -0500

    Added styling to main page, bubble, headers and banner

--- src/components/BigBubble/BigBubble.js
+++ src/components/BigBubble/BigBubble.js
@@ -1,10 +1,18 @@
 import './_BigBubble.scss';
+import { useContext } from 'react';
+import {DataContext} from '../../contexts/DataContext'
 
 const BigBubble = () => {
+  const data = useContext(DataContext)
+  const artist = data.data.mainArtist
+  console.log(artist)
   return (
-    <article className="big-bubs">
-      burbuja grande
-    </article>
+    <div className='bubble-wrap'>
+      <article className="big-bubs">
+        <img className='image' src='https://www.rockaxis.com/img/newsList/2813934.jpg'></img>
+      </article>
+      <h2>{data && <p>{artist.Name}</p>}</h2>
+    </div>
   );
 }
 
