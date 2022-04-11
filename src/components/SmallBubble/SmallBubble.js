1376fdfe90613591915555deaebf6d893600eecc
Eric Matlock <ermatlock@gmail.com>
2022-04-11 17:38:05 -0400

    Set onclick

--- src/components/SmallBubble/SmallBubble.js
+++ src/components/SmallBubble/SmallBubble.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import './_SmallBubble.scss';
+import React, { useContext } from "react";
+import "./_SmallBubble.scss";
+import { DataContext } from "../../contexts/DataContext";
 
-const SmallBubble = ({item}) => {
+const SmallBubble = ({ item, setQuery }) => {
   return (
-    <div className="little-bubs" id={item.id}>
+    <div
+      className="little-bubs"
+      id={item.id}
+      onClick={() => setQuery(item.Name)}
+    >
       <p>{item.Name}</p>
-      
     </div>
   );
-}
+};
 
 export default SmallBubble;
