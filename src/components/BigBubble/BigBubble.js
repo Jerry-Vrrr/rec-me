3fa81d1b63ce1dcde18ba1e435a4ee4827b8911a
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-12 12:50:38 -0500

    Add link to big bubble

--- src/components/BigBubble/BigBubble.js
+++ src/components/BigBubble/BigBubble.js
@@ -1,21 +1,24 @@
 import "./_BigBubble.scss";
 import { useContext } from "react";
 import { DataContext } from "../../contexts/DataContext";
+import { Route, Link } from "react-router-dom";
 
 const BigBubble = () => {
   const data = useContext(DataContext);
   const mainItem = data.data.mainItem;
   console.log(mainItem);
   return (
-    <div className="bubble-wrap">
-      <article className="big-bubs">
-        <img
-          className="image"
-          src="https://studiosol-a.akamaihd.net/letras/500x500/fotos/a/7/6/b/a76b7ee31a54dde5c98a1cc4cc620e0b.jpg"
-        ></img>
-      </article>
-      <h2>{data && <p>{mainItem.Name}</p>}</h2>
-    </div>
+    <Link to={`/${mainItem.Name}`}>
+      <div className="bubble-wrap">
+        <article className="big-bubs">
+          <img
+            className="image"
+            src="https://studiosol-a.akamaihd.net/letras/500x500/fotos/a/7/6/b/a76b7ee31a54dde5c98a1cc4cc620e0b.jpg"
+          ></img>
+        </article>
+        <h2>{data && <p>{mainItem.Name}</p>}</h2>
+      </div>
+    </Link>
   );
 };
 
