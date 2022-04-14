060a486d14139bea6a7b3e4346b428d3a02c5147
Katie Ammon <katie.ammon10@gmail.com>
2022-04-14 15:53:20 -0700

    Added SearchBar to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
 
 import React, { useState } from 'react';
 import './_Header.scss'
-import Form from '../SearchBar/SearchBar'
+import SearchBar from '../SearchBar/SearchBar'
+
+
 
 
 const Header = () => {
@@ -9,11 +11,7 @@ const Header = () => {
     <section className='header'>
       <h1>RecMe</h1>
       <div className='search-bar'>
-        <select className='select' placeholder='choose'>Choose
-         <option name='Music' value='Music'>Music</option>
-          <option name='Movie' value='Movie'>Movie</option>
-       </select>
-      <Form />
+      <SearchBar />
       </div>
       <button className='favorites' placeholder='Favorites'>Favorites</button>
     </section>
