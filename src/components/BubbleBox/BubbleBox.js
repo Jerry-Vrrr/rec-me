dcf8455b30c13079de79a0e34336724af76c919f
Eric Matlock <ermatlock@gmail.com>
2022-04-16 18:10:21 -0400

    Create didIWin conditional function

--- src/components/BubbleBox/BubbleBox.js
+++ src/components/BubbleBox/BubbleBox.js
@@ -10,13 +10,37 @@ import Loader from "../Loader/Loader"
 
 
 const BubbleBox = () => {
+  const gameInfo = useContext(GameContext)
+  const data = useContext(DataContext);
+  const relatedItems = data.data.relatedItems;
  
 const getRandomIndex = (array) => {
   return Math.floor(Math.random() * array.length);
 };
 
-  const data = useContext(DataContext);
-  const relatedItems = data.data.relatedItems;
+const didIWin = () => {
+  console.log(data.data.mainItem.Name)
+  // console.log(gameInfo.turnCounter)
+  // console.log("didIWinFire?")
+  if (!gameInfo.gameIsActive) {
+    console.log("gameisnotactive")
+    return
+  }
+   if (gameInfo.turnCounter > 1 && data.data.mainItem.Name !== gameInfo.goalArtist){
+    console.log("not yet")
+    return gameInfo.setGameMessage("Nah, son")
+  } 
+  if (gameInfo.turnCounter === 1) {
+    console.log("loser")
+    gameInfo.setGameMessage("You're a loser baby!")
+  }
+  if (gameInfo.turnCounter > 1 && data.data.mainItem.Name === gameInfo.goalArtist) {
+    console.log("winner")
+    return gameInfo.setGameMessage("You did the thing!")
+  } 
+}
+
+  
 
   useEffect(() => {
     data.setQuery(initialItems[getRandomIndex(initialItems)]);
@@ -25,7 +49,7 @@ const getRandomIndex = (array) => {
   const createBubbles = () => {
     return relatedItems.map((item, index) => {
       item.id = `bubble${index+=1}`;
-      return <SmallBubble item={item} key={index+=1} setQuery={data.setQuery} />;
+      return <SmallBubble item={item} key={index+=1} setQuery={data.setQuery} didIWin={didIWin} />;
     });
   };
 
