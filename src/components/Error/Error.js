258a9bb724e480485de041669306ea46a6efd9bf
Madison Law <madisonlaw1@gmail.com>
2022-04-15 20:27:23 -0600

    Add message to Err component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,10 +2,11 @@ import './_Error.scss';
 import { Link } from 'react-router-dom';
 
 
-const Error = () => {
+const Error = ({error}) => {
   return (
     <section className="error-container">
       <h2>Whoops! Something went wrong.</h2>
+      {error && `Whoops! Something went wrong: ${error}`}
       <Link to="/">
         <button>Back to Home</button>      
       </Link>
