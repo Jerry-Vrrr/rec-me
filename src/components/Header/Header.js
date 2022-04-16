d5daf5bc28b5339b38470f513816088220d07f48
Madison Law <madisonlaw1@gmail.com>
2022-04-15 20:58:01 -0600

    Add link to home page to logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,13 @@
 import React, { useState } from 'react';
 import './_Header.scss'
 import SearchBar from '../SearchBar/SearchBar'
-
-
+import {Link} from 'react-router-dom';
 
 
 const Header = () => {
   return (  
     <section className='header'>
-      <h1>RecMe</h1>
+      <Link to='/'><h1>RecMe</h1></Link>
       <div className='search-bar'>
       <SearchBar />
       </div>
