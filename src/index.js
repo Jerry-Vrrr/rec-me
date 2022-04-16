62843a5f386198ee1808799ff91955b7eae28d9b
Eric Matlock <ermatlock@gmail.com>
2022-04-16 12:15:41 -0400

    Wrap in datacontextprovider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import {DataContextProvider} from './contexts/DataContext'
 
 
 ReactDOM.render(
   <BrowserRouter>
+  <DataContextProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+  </DataContextProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
