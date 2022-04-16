fb155eaad403ffe9c6541412122a37a35d6d18dd
Eric Matlock <ermatlock@gmail.com>
2022-04-16 12:59:37 -0400

    Uncomment datacontextprovider

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
-import {DataContextProvider} from './contexts/DataContext'
+// import {DataContextProvider} from './contexts/DataContext'
 
 
 ReactDOM.render(
   <BrowserRouter>
-  <DataContextProvider>
+  {/* <DataContextProvider> */}
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </DataContextProvider>
+  {/* </DataContextProvider> */}
   </BrowserRouter>,
   document.getElementById('root')
 );
