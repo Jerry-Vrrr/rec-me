c5bf3eeab9f9906be5ac276b6ac5ea99445ba5bb
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-16 09:29:16 -0500

    Refactor css styling for single artist page

--- src/components/RelatedArtist/RelatedArtist.js
+++ src/components/RelatedArtist/RelatedArtist.js
@@ -3,7 +3,7 @@ import './_RelatedArtist.scss';
 const RelatedArtists = ({name, id}) => {
   return (
       <div className="related-item" id={id}>
-        <h2>{name}</h2>
+        <h2 className='related-blorb'>{name}</h2>
       </div>
   );
 }
