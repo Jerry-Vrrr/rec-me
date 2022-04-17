8f32a4eda588d9f72309725b4d9fb2f8d56acb12
Madison Law <madisonlaw1@gmail.com>
2022-04-17 15:37:07 -0600

    Add 5xx error test

--- cypress/integration/main-dashboard-spec.js
+++ cypress/integration/main-dashboard-spec.js
@@ -53,24 +53,24 @@ describe('Main page flow', () => {
         .get('.banner').find('img').should('be.visible')
     });
 
-    // it('should display bubbles', () => {
-    //     // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
-    //     cy.visit('http://localhost:3000/')
-    //     cy.wait(5000)
-    //     .get('.big-bubs').find('img').should('be.visible')
-    //     .get('.bubble-wrap').find('p').should('be.visible')
-    //     .get('.little-bubs').get('#bubble5').find('h4').should('be.visible')
+    it('should display bubbles', () => {
+        // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
+        cy.visit('http://localhost:3000/')
+        cy.wait(5000)
+        .get('.big-bubs').find('img').should('be.visible')
+        .get('.bubble-wrap').find('p').should('be.visible')
+        .get('.little-bubs').get('#bubble5').find('h4').should('be.visible')
         
-    // });
+    });
 
-    // it('should have clickable bubbles ', () => {
-    //     // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
-    //     cy.visit('http://localhost:3000/')
-    //     cy.wait(5000)
-    //     .get('.little-bubs').get('#bubble5')
-    //     .click()
-    //     .get('.bubble-wrap').find('h2').should('be.visible')
-    // });
+    it('should have clickable bubbles ', () => {
+        // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
+        cy.visit('http://localhost:3000/')
+        cy.wait(5000)
+        .get('.little-bubs').get('#bubble5')
+        .click()
+        .get('.bubble-wrap').find('h2').should('be.visible')
+    });
 
     it('should display a footer', () => {
         cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
@@ -78,23 +78,31 @@ describe('Main page flow', () => {
         .get('.footer')
     });
 
-    // it('should allow user to click on main artist and route to a new page', () => {
-    //     // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
-    //     cy.visit('http://localhost:3000/')
-    //       .get('.bubble-wrap')
-    //       cy.wait(2000)
-    //       .get('.big-bubs').click()
-    //       .get('iframe')
-    // });
+    it('should allow user to click on main artist and route to a new page', () => {
+        // cy.intercept('GET', 'http://fe-cors-proxy.herokuapp.com', interceptData)
+        cy.visit('http://localhost:3000/')
+          .get('.bubble-wrap')
+          cy.wait(2000)
+          .get('.big-bubs').click()
+          .get('iframe')
+    });
 
 
-    it("should display error message to user when server is down", () => {
+    it("should display error message to user when encountering 422 error", () => {
         cy.intercept("http://fe-cors-proxy.herokuapp.com", {
           statusCode: 422,
         }).as("422 error");
         cy.visit("http://localhost:3000/")
         .contains('Whoops! Something went wrong.')
       });
+
+    it("should display error message to user when encountering 500 error", () => {
+        cy.intercept("http://fe-cors-proxy.herokuapp.com", {
+            statusCode: 500,
+        }).as("500 error");
+        cy.visit("http://localhost:3000/")
+        .contains('Whoops! Something went wrong.')
+        });
 
 
 
