2c46aecbd7a1ce734a860bd2c2d94c9d696e4d61
Katie Ammon <katie.ammon10@gmail.com>
2022-04-17 09:27:50 -0700

    Added something here too

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,7 +1,7 @@
 import React, { useState, createContext, useEffect  } from "react";
 import { fetchData } from "../apiCalls";
 import Error from "../components/Error/Error";
-import initialItems from "../data";
+import {initialItems, gameArtists} from "../data";
 
 const GameContext = createContext();
 
@@ -17,14 +17,14 @@ const GameContextProvider = ({ children }) => {
 
   const [turnCounter, setTurnCounter] = useState(6)
   
-  const [goalArtist, setGoalArtist] = useState(initialItems[getRandomIndex(initialItems)])
+  const [goalArtist, setGoalArtist] = useState(gameArtists[getRandomIndex(gameArtists)])
 
   const [gameIsActive, setGameIsActive] = useState(true)
 
   const [gameMessage, setGameMessage] = useState("")
 
   return (
-    <GameContext.Provider value={{ turnCounter, setTurnCounter, goalArtist, gameIsActive, gameReset, gameMessage, setGameMessage }}>
+    <GameContext.Provider value={{ turnCounter, setTurnCounter, goalArtist, gameIsActive, gameReset, gameMessage, setGameMessage, setGameIsActive }}>
       {children}
     </GameContext.Provider>
   );
