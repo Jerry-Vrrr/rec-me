3b427962fec42ff111ff6d62c88250b8a9381cfd
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-17 20:12:28 -0500

    Remove searchbar while game is active or on single artist page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { Link, useLocation  } from 'react-router-dom';
 import './_Header.scss'
 import SearchBar from '../SearchBar/SearchBar'
+import { GameContext } from '../../contexts/GameContext';
 
 
 const Header = () => {
   const location = useLocation()
-  
+  const gameInfo = useContext(GameContext)
   return (  
     <section className='header'>
       <Link to="/">
@@ -14,7 +15,7 @@ const Header = () => {
       </Link>
       
       <div className='search-bar-container'>
-      {location.pathname === '/' && <SearchBar />}
+      {location.pathname === '/' && !gameInfo.gameIsActive && <SearchBar />}
       </div>
       
     </section>
