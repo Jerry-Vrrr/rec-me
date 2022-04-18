c82939f115ceac74625e80019bf4751125b7369a
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:43:50 -0500

    Final Refactor

--- src/components/BigBubble/BigBubble.js
+++ src/components/BigBubble/BigBubble.js
@@ -9,30 +9,37 @@ const BigBubble = () => {
   const data = useContext(DataContext);
   const mainItem = data.data.mainItem;
 
-  const [image, setImage] = useState(null)
+  const [image, setImage] = useState(null);
   useEffect(() => {
-    fetchImages(mainItem.Name).then(imageInfo => {
-        setImage(imageInfo.thumb_url)
-    }).catch((err) => {
-      <Error error={err.message}/>
-    })
-  }, [mainItem.Name])
-  
+    fetchImages(mainItem.Name)
+      .then((imageInfo) => {
+        setImage(imageInfo.thumb_url);
+      })
+      .catch((err) => {
+        <Error error={err.message} />;
+      });
+  }, [mainItem.Name]);
+
   return (
     <Link to={`/artists/${mainItem.Name}`}>
-    <div className="big-bub-container">
-      {image && <div className="bubble-wrap scale-in-center">
-        <article className="big-bubs" onClick={() => {
-        data && data.setQuery(mainItem.Name)}}>
-        {image === "https://photos.bandsintown.com/artistThumb.jpg" || !image ? 
-        null : 
-        <img className="image" src={image}/>
-        }
-        </article>
-        <h2>{data && <p className='big-bub-name'>{mainItem.Name}</p>}</h2>
-      </div>}
-
-    </div>
+      <div className="big-bub-container">
+        {image && (
+          <div className="bubble-wrap scale-in-center">
+            <article
+              className="big-bubs"
+              onClick={() => {
+                data && data.setQuery(mainItem.Name);
+              }}
+            >
+              {image === "https://photos.bandsintown.com/artistThumb.jpg" ||
+              !image ? null : (
+                <img className="image" src={image} />
+              )}
+            </article>
+            <h2>{data && <p className="big-bub-name">{mainItem.Name}</p>}</h2>
+          </div>
+        )}
+      </div>
     </Link>
   );
 };
