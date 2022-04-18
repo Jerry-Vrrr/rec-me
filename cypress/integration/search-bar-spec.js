2ec8b90c764979cdf4b26e9623e85d0038d4816a
Katie Ammon <katie.ammon10@gmail.com>
2022-04-18 13:46:00 -0600

    Update className and refactor

--- cypress/integration/search-bar-spec.js
+++ cypress/integration/search-bar-spec.js
@@ -4,10 +4,8 @@ describe('Search artist flow', () => {
     cy.visit('http://localhost:3000/')
     cy.wait(2000)
     .get('.search-bar').type('Beyonce')
-    .get('.submit').click()
+    .get('.button-search').click()
     cy.wait(5000)
     .get('.image').should('have.attr', 'src').should('include', 'https://photos.bandsintown.com/thumb/8652941.jpeg')
-});
-
-
+  });
 })
