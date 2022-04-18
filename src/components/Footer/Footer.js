f852d0b9f43aa7235a5908600fbdfa09aaa63e6b
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:44:14 -0500

    Final Refactor

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,17 @@
-import './_Footer.scss'
-import React from 'react';
+import "./_Footer.scss";
+import React from "react";
 
 const Footer = () => {
-  return (  
-    <section className='footer'>
-      
-      <h4> About - Sources - Contact - Maddy - Katie - Eric - Jerry </h4>
-
+  return (
+    <section className="footer">
+      <h4>
+        Contact Us! <a href="https://www.linkedin.com/in/mlaw1/">Maddie</a>
+        <a href="https://www.linkedin.com/in/kammon10/">Katie</a>
+        <a href="https://www.linkedin.com/in/ericmatlock/">Eric</a>
+        <a href="https://www.linkedin.com/in/jerry-vohrer-974a97205/">Jerry</a>
+      </h4>
     </section>
-    
   );
-}
- 
+};
+
 export default Footer;
