1d0889d5e7bc2f276850c5fd4a43be1d8718f112
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:44:33 -0500

    Final Refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,24 @@
-import { useState, useContext } from 'react';
-import { Link, useLocation  } from 'react-router-dom';
-import './_Header.scss'
-import SearchBar from '../SearchBar/SearchBar'
-import { GameContext } from '../../contexts/GameContext';
-import logo from "../../images/recme-logo.svg"
-
+import { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./_Header.scss";
+import SearchBar from "../SearchBar/SearchBar";
+import { GameContext } from "../../contexts/GameContext";
+import logo from "../../images/recme-logo.svg";
 
 const Header = () => {
-  const location = useLocation()
-  const gameInfo = useContext(GameContext)
-  return (  
-    <section className='header'>
+  const location = useLocation();
+  const gameInfo = useContext(GameContext);
+  return (
+    <section className="header">
       <Link to="/">
-       <img className="logo" src={logo} alt="RecMe stylized logo"/>      
+        <img className="logo" src={logo} alt="RecMe stylized logo" />
       </Link>
-      
-      <div className='search-bar-container'>
-      {location.pathname === '/' && !gameInfo.gameIsActive && <SearchBar />}
+
+      <div className="search-bar-container">
+        {location.pathname === "/" && !gameInfo.gameIsActive && <SearchBar />}
       </div>
-      
     </section>
   );
-}
- 
+};
+
 export default Header;
-
-
