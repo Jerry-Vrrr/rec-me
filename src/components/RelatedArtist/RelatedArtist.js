2c4a9670894cef45a404be01241f0284b5a6a21e
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:44:56 -0500

    Final Refactor

--- src/components/RelatedArtist/RelatedArtist.js
+++ src/components/RelatedArtist/RelatedArtist.js
@@ -1,11 +1,11 @@
-import './_RelatedArtist.scss';
+import "./_RelatedArtist.scss";
 
-const RelatedArtists = ({name, id}) => {
+const RelatedArtists = ({ name, id }) => {
   return (
-      <div className="related-item" id={id} key={id}>
-        <h2 className='related-blorb'>{name}</h2>
-      </div>
+    <div className="related-item" id={id} key={id}>
+      <h2 className="related-blorb">{name}</h2>
+    </div>
   );
-}
+};
 
 export default RelatedArtists;
