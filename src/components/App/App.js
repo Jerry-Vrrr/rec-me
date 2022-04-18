7be5c43b698883baba41a97bdac401ff79a04e98
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:43:41 -0500

    Final Refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,60 +1,47 @@
-import React, { useEffect, useContext } from 'react';
-import '../App/_App.scss';
-import { Route, Switch } from 'react-router-dom';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import BubbleBox from '../BubbleBox/BubbleBox';
-import { fetchData } from '../../apiCalls';
-import {DataContextProvider} from '../../contexts/DataContext'
-import SingleArtist from '../SingleArtist/SingleArtist';
-import Error from '../Error/Error';
-import { GameContextProvider } from '../../contexts/GameContext';
-// import initialItems from "../../data"
-
+import React from "react";
+import "../App/_App.scss";
+import { Route, Switch } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import BubbleBox from "../BubbleBox/BubbleBox";
+import { DataContextProvider } from "../../contexts/DataContext";
+import SingleArtist from "../SingleArtist/SingleArtist";
+import Error from "../Error/Error";
+import { GameContextProvider } from "../../contexts/GameContext";
 
 const App = () => {
-  // const data = useContext(DataContext);
-
-  // const getRandomIndex = (array) => {
-  //   return Math.floor(Math.random() * array.length);
-  // };
-  
-  // useEffect(() => {
-  //   data.setQuery(initialItems[getRandomIndex(initialItems)]);
-  // }, []);
-
   return (
     <div className="App">
       <DataContextProvider>
-      <GameContextProvider>
-        <Header />
+        <GameContextProvider>
+          <Header />
 
-        <Switch>
-          <Route exact path='/' render={() => {
-            return (
-              
-                <BubbleBox />      
-     
-            )
-          }}/>
-      
-          <Route exact path='/artists/:Name' render={({match}) => {
-            return (         
-              <SingleArtist name={match.params.Name} />         
-            )
-          }}/>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return <BubbleBox />;
+              }}
+            />
 
-          <Route>
-            <Error />
-          </Route>
-    
-        </Switch>
+            <Route
+              exact
+              path="/artists/:Name"
+              render={({ match }) => {
+                return <SingleArtist name={match.params.Name} />;
+              }}
+            />
+
+            <Route>
+              <Error />
+            </Route>
+          </Switch>
         </GameContextProvider>
-     
       </DataContextProvider>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
