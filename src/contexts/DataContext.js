5322dd2202bfdcf114db4322c90531566af9c97b
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-04-18 15:45:35 -0500

    Final Refactor

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,42 +1,40 @@
-import React, { useState, createContext, useEffect  } from "react";
+import React, { useState, createContext, useEffect } from "react";
 import { fetchData } from "../apiCalls";
 import Error from "../components/Error/Error";
-import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
 const DataContext = createContext();
 const DataContextProvider = ({ children }) => {
-
   const [data, setData] = useState(null);
   const [searchQuery, setQuery] = useState("");
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   /*dynamic fetch query*/
   useEffect(() => {
-    setIsLoading(true)
-    fetchData(searchQuery).then((info) => {
-      setData({
-        mainItem: info.Similar.Info[0],
-        relatedItems: info.Similar.Results,
-      });
-    }).catch((err) => {
-      setError(true)
-    })
-      .finally(() => setIsLoading(false))
+    setIsLoading(true);
+    fetchData(searchQuery)
+      .then((info) => {
+        setData({
+          mainItem: info.Similar.Info[0],
+          relatedItems: info.Similar.Results,
+        });
+      })
+      .catch((err) => {
+        setError(true);
+      })
+      .finally(() => setIsLoading(false));
   }, [searchQuery]);
 
-  
   return (
     <DataContext.Provider value={{ data, setQuery, isLoading, error }}>
       {data && children}
-      {!data && error ? 
+      {!data && error ? (
         <React.Fragment>
           <Header />
           <Error />
-        </React.Fragment> : null
-      }
-      
+        </React.Fragment>
+      ) : null}
     </DataContext.Provider>
   );
 };
