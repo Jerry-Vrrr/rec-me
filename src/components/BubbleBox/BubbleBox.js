383c28e998816e63d76803915c10e035168685cc
Eric Matlock <ermatlock@gmail.com>
2022-04-18 20:37:13 -0400

    Remove useState

--- src/components/BubbleBox/BubbleBox.js
+++ src/components/BubbleBox/BubbleBox.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import "./_BubbleBox.scss";
 import BigBubble from "../BigBubble/BigBubble";
 import { DataContext } from "../../contexts/DataContext";
