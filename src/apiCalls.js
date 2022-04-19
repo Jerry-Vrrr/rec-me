279e21a9f8cca7bfb4eb31e4278a1ebf15bbb492
Eric Matlock <ermatlock@gmail.com>
2022-04-18 20:36:03 -0400

    Add s to http for proxy servers

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const fetchData = async (query) => {
-  return fetch("http://fe-cors-proxy.herokuapp.com", {
+  return fetch("https://fe-cors-proxy.herokuapp.com", {
     headers: {
       "Target-URL": `https://tastedive.com/api/similar?k=435194-ConcertT-B82P7E7L&info=1&q=${query}`,
     },
@@ -13,7 +13,7 @@ export const fetchData = async (query) => {
 };
 
 export const fetchImages = async (name) => {
-  return fetch("http://fe-cors-proxy.herokuapp.com", {
+  return fetch("https://fe-cors-proxy.herokuapp.com", {
     headers: {
       "Target-URL": `https://rest.bandsintown.com/artists/${name}/?app_id=9249d4efca2fc99c7b6258541ce41a4f`,
     },
